be42e1c3aa9728d0cd414268ccdb517fe15ad49c
Miguel de Val-Borro <miguel.deval@gmail.com>
2011-07-14 18:49:31 +0200

    Save figure to png file

--- flash-3d.py
+++ flash-3d.py
@@ -17,6 +17,11 @@ parser = ArgumentParser()
 parser.add_argument("-f", "--file", dest="filename", help="Input file to read data from")
 args = parser.parse_args()
 
+mirahdf5 = flashhdf5.FlashHDF5('/mnt/cdrom/mako/data6/mira-wind-70AU/t-3e2/mira-wind-70AU_hdf5_plt_cnt_0412')
+dens = np.log10(mirahdf5.get_var('dens'))
+r, t = np.meshgrid(mirahdf5.x, np.append(mirahdf5.y, mirahdf5.y[-1]+mirahdf5.dy_fine))
+xarr = r*np.cos(t)
+yarr = r*np.sin(t)
 hdf5 = flashhdf5.FlashHDF53D(args.filename)
 plot_var = np.log10(hdf5.get_var('dens'))
 print plot_var.shape
@@ -24,11 +29,13 @@ print plot_var.shape
 # plt.show()
 # mlab.contour3d(plot_var)
 # mlab.pipeline.volume(mlab.pipeline.scalar_field(plot_var), vmin=0.2, vmax=0.8)
-mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(plot_var),
-                                    plane_orientation='x_axes',
-                                    slice_index=40)
-mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(plot_var),
-                                    plane_orientation='y_axes',
-                                    slice_index=20)
+mlab.surf(dens, warp_scale="auto")
+# mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(dens))
+#                                     plane_orientation='x_axes',
+#                                     slice_index=40)
+# mlab.pipeline.image_plane_widget(mlab.pipeline.scalar_field(plot_var),
+#                                     plane_orientation='y_axes',
+#                                     slice_index=20)
 mlab.outline()
+# mlab.savefig('Mira.png', size=(400,300))
 mlab.show()
